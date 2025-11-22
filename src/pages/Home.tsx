c3284e54a7d19a1daf43c7e2ffde29c85335aa98
Gabriel Teixeira <gabrielm.teixeira@ua.pt>
2025-11-22 19:51:19 +0000

    Melhor separação da pagina Home e Inico da ideia dos favoritos

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,20 +1,31 @@
-import { Link } from 'react-router-dom'
+import { Link } from "react-router-dom";
+import "../styles/globals.css";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Digidex</h1>
-      <p>Explora Digimon e Pokémon usando APIs públicas.</p>
+    <div className="fullpage-root">
+      <Link to="/digimon" className="fullpage-strip left">
+        <img src="/src/assets/digimon.gif" className="hover-gif left-gif" />
+        <div className="panel-content">
+          <h2>Digimon</h2>
+        </div>
+      </Link>
 
-      <div>
-        <Link to="/digimon">
-          <button>Ir para Digimon</button>
-        </Link>
+      <div className="fullpage-strip center">
+        <div className="panel-content">
+          <h2>ASH & TAI</h2>
+          <h4>ESCOLHE OS TEUS FAVORITOS</h4>
 
-        <Link to="/pokemon" style={{ marginLeft: '10px' }}>
-          <button>Ir para Pokémon</button>
-        </Link>
+          <h4>AQUI APREES ODOS OS FAVORIOTS</h4>
+        </div>
       </div>
+
+      <Link to="/pokemon" className="fullpage-strip right">
+        <img src="/src/assets/pokemon.gif" className="hover-gif right-gif" />
+        <div className="panel-content">
+          <h2>Pokémon</h2>
+        </div>
+      </Link>
     </div>
-  )
+  );
 }
