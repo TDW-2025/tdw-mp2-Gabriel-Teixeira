465fb80d51b8cad6f40a72a4f11542616f72115d
Gabriel Teixeira <gabrielm.teixeira@ua.pt>
2025-11-24 15:57:54 +0000

    Correção da digimon array

--- src/pages/DigimonDetail.tsx
+++ src/pages/DigimonDetail.tsx
@@ -5,23 +5,23 @@ import type { RootState } from "../store";
 import { HeartStraight } from "phosphor-react";
 import { useGetDigimonByNameQuery } from "../services/digimonApi";
 import styles from "../styles/DigimonDetail.module.css";
-import stylesGolbal from "../styles/Global.module.css";
+import stylesGlobal from "../styles/Global.module.css";
+
 
 export default function DigimonDetail() {
-  const { name } = useParams();
+  const { name } = useParams<{ name: string }>();
   const navigate = useNavigate();
   const decodedName = name ? decodeURIComponent(name) : "";
 
   const favoritos = useSelector((state: RootState) => state.favoritos.items);
   const dispatch = useDispatch();
 
-  const { data, isLoading, isError } = useGetDigimonByNameQuery(decodedName);
+  // data: Digimon | undefined
+  const { data: digimon, isLoading, isError } = useGetDigimonByNameQuery(decodedName);
 
   if (isLoading) return <p className={styles.message}>Carregando detalhes...</p>;
   if (isError) return <p className={styles.message}>Erro ao carregar detalhes.</p>;
-  if (!data || data.length === 0) return <p className={styles.message}>Digimon não encontrado.</p>;
-
-  const digimon = data[0];
+  if (!digimon) return <p className={styles.message}>Digimon não encontrado.</p>;
 
   const toggleFavorito = () => {
     if (favoritos.includes(digimon.name)) dispatch(removeFavorito(digimon.name));
@@ -29,12 +29,12 @@ export default function DigimonDetail() {
   };
 
   return (
-    <div className={stylesGolbal.container}>
-      <button className={stylesGolbal.backButton} onClick={() => navigate(-1)}>
+    <div className={stylesGlobal.container}>
+      <button className={stylesGlobal.backButton} onClick={() => navigate(-1)}>
         ← Voltar
       </button>
 
-      <div className={stylesGolbal.card}>
+      <div className={stylesGlobal.card}>
         <h1 className={styles.name}>{digimon.name}</h1>
 
         <img src={digimon.img} alt={digimon.name} className={styles.image} />
