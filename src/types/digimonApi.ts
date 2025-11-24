a9507fc0c73b8a821cdccd71eba6918ae8cf067a
Gabriel Teixeira <gabrielm.teixeira@ua.pt>
2025-11-24 15:50:07 +0000

    Corrige tipos TypeScript em APIs Digimon e Pokémon e remove any

--- src/types/digimonApi.ts
+++ src/types/digimonApi.ts
@@ -1,23 +1,3 @@
-// import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-
-// export const digimonApi = createApi({
-//   reducerPath: "digimonApi",
-//   baseQuery: fetchBaseQuery({
-//     baseUrl: "https://digimon-api.vercel.app/api/",
-//   }),
-//   endpoints: (builder) => ({
-//     getDigimonList: builder.query<any[], void>({
-//       query: () => "digimon",
-//     }),
-
-//     getDigimonByName: builder.query<any, string>({
-//       query: (name) => `digimon/name/${name}`,
-//     }),
-//   }),
-// });
-
-// export const { useGetDigimonListQuery, useGetDigimonByNameQuery } = digimonApi;
-
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import type { Digimon } from "../types/digimon";
 
@@ -30,6 +10,7 @@ export const digimonApi = createApi({
     getDigimonList: builder.query<Digimon[], void>({
       query: () => "digimon",
     }),
+
     getDigimonByName: builder.query<Digimon, string>({
       query: (name) => `digimon/name/${name}`,
     }),
