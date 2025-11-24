13c1169dadba00e8057fc3f99bea0be014d37ecd
Gabriel Teixeira <gabrielm.teixeira@ua.pt>
2025-11-24 22:04:16 +0000

    Separação de div, melhor css

--- src/pages/PokemonDetail.tsx
+++ src/pages/PokemonDetail.tsx
@@ -14,40 +14,45 @@ export default function PokemonDetail() {
   const pokemon: Pokemon = data 
 
   return (
-    <div className={stylesGlobal.containerPokemom}>
-      <h1 className={styles.title}>{pokemon.name}</h1>
+   <div className={stylesGlobal.containerPokemom}>
+  <h1 className={styles.title}>{pokemon.name}</h1>
 
-      <div className={styles.card}>
-        <img src={pokemon.sprites.front_default} alt={pokemon.name} className={styles.img} />
-        <img src={pokemon.sprites.back_default} alt={pokemon.name} className={styles.img} />
+  <div className={styles.card}>
+    <div className={styles.images}>
+      <img src={pokemon.sprites.front_default} alt={pokemon.name} className={styles.img} />
+      <img src={pokemon.sprites.back_default} alt={pokemon.name} className={styles.img} />
+    </div>
 
-        <p><strong>Altura:</strong> {pokemon.height / 10} m</p>
-        <p><strong>Peso:</strong> {pokemon.weight / 10} kg</p>
+    <p><strong>Altura:</strong> {pokemon.height / 10} m</p>
+    <p><strong>Peso:</strong> {pokemon.weight / 10} kg</p>
 
-        <div className={styles.types}>
-          {pokemon.types.map((t) => (
-            <span key={t.type.name} className={styles.type}>{t.type.name}</span>
-          ))}
-        </div>
+    <div className={styles.types}>
+      {pokemon.types.map((t) => (
+        <span key={t.type.name} className={styles.type}>{t.type.name}</span>
+      ))}
+    </div>
 
-        <div className={styles.stats}>
-          <h3>Estatísticas</h3>
-          {pokemon.stats.map((s) => (
-            <p key={s.stat.name} className={styles.stat}>
-              {s.stat.name}: {s.base_stat}
-            </p>
-          ))}
-        </div>
+    <div className={styles.infoBoxes}>
+      <div className={styles.stats}>
+        <h3>Estatísticas</h3>
+        {pokemon.stats.map((s) => (
+          <p key={s.stat.name} className={styles.stat}>
+            {s.stat.name}: {s.base_stat}
+          </p>
+        ))}
+      </div>
 
-        <div className={styles.abilities}>
-          <h3>Habilidades</h3>
-          {pokemon.abilities.map((a) => (
-            <span key={a.ability.name} className={styles.ability}>
-              {a.ability.name}
-            </span>
-          ))}
-        </div>
+      <div className={styles.abilities}>
+        <h3>Habilidades</h3>
+        {pokemon.abilities.map((a) => (
+          <span key={a.ability.name} className={styles.ability}>
+            {a.ability.name}
+          </span>
+        ))}
       </div>
     </div>
+  </div>
+</div>
+
   )
 }
