1885d0e83cfe9ee9ee1d339a35e804c043f276d3
Gabriel Teixeira <gabrielm.teixeira@ua.pt>
2025-11-26 16:11:05 +0000

    css e dependencias tailwind

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,48 @@
+// import { Link } from "react-router-dom";
+// import { useGetDigimonListQuery } from "../services/digimonApi";
+// import FavoritesPanel from "../componentes/FavoritesPanel";
+// import { useSelector } from "react-redux";
+// import type { RootState } from "../store";
+// import styles from "../styles/Home.module.css";
+
+// export default function Home() {
+//   const favoritos = useSelector((state: RootState) => state.favoritos.items);
+
+//   const { data: digimons } = useGetDigimonListQuery();
+//   const favoritosDigimon = digimons
+//     ?.filter((d) => favoritos.includes(d.name))
+//     .map((d) => ({
+//       nome: d.name,
+//       imagem: d.img,
+//       tipo: "digimon" as const,
+//     })) ?? [];
+
+//   return (
+//     <div className={styles.fullpageRoot}>
+      
+//       <Link to="/digimon" className={`${styles.fullpageStrip} ${styles.left}`}>
+//         <img src="./src/assets/digimon.gif" className={styles.hoverGif} />
+//         <div className={styles.panelContent}>
+//           <h2>Digimon</h2>
+//           <button>Escolhe o teu digimon</button>
+//         </div>
+//       </Link>
+
+//       <div className={`${styles.fullpageStrip} ${styles.center}`}>
+//         <FavoritesPanel favoritos={favoritosDigimon} />
+//       </div>
+
+//       <Link to="/pokemon" className={`${styles.fullpageStrip} ${styles.right}`}>
+//         <img src="./src/assets/pokemon.gif" className={styles.hoverGif} />
+//         <div className={styles.panelContent}>
+//           <h2>Pokémon</h2>
+//           <button>Escolhe o teu pokemon</button>
+//         </div>
+//       </Link>
+//     </div>
+//   );
+// }
+
 import { Link } from "react-router-dom";
 import { useGetDigimonListQuery } from "../services/digimonApi";
 import FavoritesPanel from "../componentes/FavoritesPanel";
@@ -19,9 +64,8 @@ export default function Home() {
 
   return (
     <div className={styles.fullpageRoot}>
-      
       <Link to="/digimon" className={`${styles.fullpageStrip} ${styles.left}`}>
-        <img src="./src/assets/digimon.gif" className={styles.hoverGif} />
+        <img src="./src/assets/digimon.gif" className={styles.hoverGif} alt="Digimon GIF"/>
         <div className={styles.panelContent}>
           <h2>Digimon</h2>
           <button>Escolhe o teu digimon</button>
@@ -33,7 +77,7 @@ export default function Home() {
       </div>
 
       <Link to="/pokemon" className={`${styles.fullpageStrip} ${styles.right}`}>
-        <img src="./src/assets/pokemon.gif" className={styles.hoverGif} />
+        <img src="./src/assets/pokemon.gif" className={styles.hoverGif} alt="Pokémon GIF"/>
         <div className={styles.panelContent}>
           <h2>Pokémon</h2>
           <button>Escolhe o teu pokemon</button>
